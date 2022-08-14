21809d6e55ea7d193b5a63193538ca1683ab4e3b
Maxim Konev <sayberix.ru@yandex.ru>
2022-08-14 12:15:10 +0700

    Задача по поиОпределение нарцистического по поиску нарцистического числа - закончена!

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -6,7 +6,25 @@ namespace Codewars
     {
         static void Main(string[] args)
         {
-            // Решенные задачи для Codewars 
+            // Определение нарцистического числа
+            int n = 465;    // Исходное значение, для которого будем искать нарцистическое число
+            int nn = n;
+
+            int degree = 0;
+
+            while (n != 0)
+            {
+                n /= 10;
+                degree++;
+            }
+
+            double sumNarc = 0;
+            for (int i = 0; i < degree; i++)
+            {
+                sumNarc += Math.Pow(nn % 10, degree);
+                nn /= 10;
+            }
+            Console.WriteLine(sumNarc);
         }
     }
 }
