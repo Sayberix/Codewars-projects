ed8342120d6d1a83922f19b7618f63a54347fd93
Maxim Konev <sayberix.ru@yandex.ru>
2022-08-16 20:19:32 +0700

    Условия третьей задачи

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -4,31 +4,14 @@ namespace Codewars
 {
     internal class Program
     {
-        // Банкоматы позволяют использовать 4- или 6-значные PIN-коды, а PIN-коды не могут содержать ничего, кроме ровно 4 или ровно 6 цифр.
-        // Если функции передана допустимая строка PIN-кода, верните true, иначе верните false.
+        // Вам дадут слово. Ваша задача — вернуть средний символ слова. Если длина слова нечетная, вернуть средний символ.
+        // Если длина слова четная, верните средние 2 символа.
+
         private static void Main(string[] args)
         {
-            Console.WriteLine(ValidatePin(""));
+           
         }
 
-        public static bool ValidatePin(string pin)
-        {
-            bool validate = true;
-            int length = pin.Length;
-            if (length == 4 || length == 6)
-            {
-                for (int i = 0; i < length; i++)
-                {
-                    if (!Char.IsDigit(pin[i]))
-                    {
-                        validate = false;
-                        break;
-                    }
-                }
-            }
-            else
-                validate = false;
-            return (validate);
-        }
+
     }
 }
