f334a3f9dab5d5294e5580ecbd5228e975b2b7de
Maxim Konev <sayberix.ru@yandex.ru>
2022-08-21 00:07:17 +0700

    задача выполнена. Но главный контроль не пройден. Возможно нужны еще доработка

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -13,13 +13,46 @@ namespace Codewars
 
         private static void Main(string[] args)
         {
-
+            Console.WriteLine(ValidParentheses("(c(b(a)))(d)"));    //   hi(hi))(   ())(()  (c(b(a)))(d)
         }
 
         public static bool ValidParentheses(string input)
         {
-            // Your code here
-            return false;
+            //Console.WriteLine(${ })
+            bool resault = false;
+            if (input is not null)
+            {
+                int countLit = input.Length;
+                if (countLit > 0)
+                {
+                    int leftParentheses = 0, rightParentheses = 0;
+                    bool controlFlag = true;
+                    for (int i = 0; i < countLit; i++)
+                    {
+                        if (input[i].Equals('('))
+                        {
+                            leftParentheses++;
+                        }
+                        else
+                        {
+                            if (input[i].Equals(')'))
+                            {
+                                rightParentheses++;
+                                if (rightParentheses > leftParentheses)
+                                {
+                                    controlFlag = false;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    if (controlFlag == true && leftParentheses == rightParentheses && (leftParentheses + rightParentheses > 0))
+                        resault = true;
+                }
+                return resault;
+            }
+            else
+                return resault;
         }
     }
 }
