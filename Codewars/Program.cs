e2eb9022bc48eb5a46b229914930d7378b91a2fb
Maxim Konev <sayberix.ru@yandex.ru>
2022-08-21 22:56:06 +0700

    задача выполнена. но контроль на https://www.codeabbey.com/ не пройден по первому выводному параметру

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -9,7 +9,7 @@ namespace Codewars
 
         private static void Main(string[] args)
         {
-            int[] array = new int[] { 3, 1, 4, 1, 5, 9, 2, 6 };
+            int[] array = new int[] { 3, 16, 11, 23, 17, 21, 4, 5, 18, 20, 15, 22, 13, 10, 1, 9, 2, 8, 7, 19, 12, 14, 6 };
 
             BubbleSortOnce(array);
             /*Console.WriteLine($"[{String.Join(" ", BubbleSortOnce(array))}]");
@@ -17,25 +17,29 @@ namespace Codewars
 
         }
 
-        public static int[] BubbleSortOnce(int[] input)
+        public static void BubbleSortOnce(int[] input)
         {
             int length = input.Length;
             int[] array = new int[length];
             Array.Copy(input, array, length);
+            int passes = 0;
+            int changes = 0;
             for (int i = 0; i < length; i++)
             {
+                passes++;
                 for (int j = i + 1; j < length; j++)
                 {
-                    if(array[i] > array[j])
+                    if (array[i] > array[j])
                     {
+                        changes++;
                         int temp = array[i];
                         array[i] = array[j];
                         array[j] = temp;
                     }
                 }
             }
-
-            return array;
+            Console.Write(passes + " " + changes);
+            //return array;
         }
 
     }
