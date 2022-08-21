bcc7f56dbfe2242fac1750c9b464f12f0f8ac940
Maxim Konev <sayberix.ru@yandex.ru>
2022-08-21 21:49:37 +0700

    вариант для сайта https://www.codeabbey.com/

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -9,14 +9,33 @@ namespace Codewars
 
         private static void Main(string[] args)
         {
+            int[] array = new int[] { 3, 1, 4, 1, 5, 9, 2, 6 };
+
+            BubbleSortOnce(array);
+            /*Console.WriteLine($"[{String.Join(" ", BubbleSortOnce(array))}]");
+            Console.WriteLine($"[{String.Join(";", array)}]");*/
 
         }
 
         public static int[] BubbleSortOnce(int[] input)
         {
-            // Code the Bubblesort Algorithm here :D
+            int length = input.Length;
+            int[] array = new int[length];
+            Array.Copy(input, array, length);
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    if(array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
 
-            return ?;
+            return array;
         }
 
     }
