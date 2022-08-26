1a282e1225969546ae0d9b7c55dc761c9483f53c
Maxim Konev <sayberix.ru@yandex.ru>
2022-08-26 15:48:52 +0700

    реализация без сортировки

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Codewars
 {
@@ -7,8 +8,8 @@ namespace Codewars
         // https://www.codewars.com/kata/55c45be3b2079eccff00010f/csharp
         // Ваша задача — отсортировать заданную строку. Каждое слово в строке будет содержать одно число.
         // Это число и есть позиция, которую должно занимать слово в результате.
-        // Примечание.Цифры могут быть от 1 до 9. Таким образом, первым словом будет 1 (а не 0).
-        // Если входная строка пуста, вернуть пустую строку.Слова во входной строке будут содержать только допустимые последовательные числа.
+        // Примечание. Цифры могут быть от 1 до 9. Таким образом, первым словом будет 1 (а не 0).
+        // Если входная строка пуста, вернуть пустую строку. Слова во входной строке будут содержать только допустимые последовательные числа.
         // "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
         // "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
         // ""  -->  ""
@@ -18,21 +19,48 @@ namespace Codewars
             Order("is2 Thi1s T4est 3a");
         }
 
-        public static string Order(string words)
+        public static void Order(string words)
         {
-            int length = words.Length;
-            int count = 0;
-            for (int i = 0; i < length; i++)
+            if (words is null)
+                throw new NotImplementedException(null);
+            else
             {
-                if (Char.IsDigit(words[i]))
-                    count++;
-            }
-            const int countArray = count;
-            string [] array = new string [countArray]
-            {
+                int length = words.Length;
+                List<wordList> myList = new List<wordList>();
+                string word = null;
+                for (int i = 0; i < length; i++)
+                {
+                    word = String.Concat(word, words[i]);
+                    Console.WriteLine(word + ", " + words[i]);
+                    if (words[i].Equals(' '))
+                    {
+                        myList.Add(new wordList { word = word, number = FindNumber(word) });
+                        word = null;
+                    }
+                }
 
+                string FindNumber(string wordInFunction)
+                {
+                    for (int i = 0; i < wordInFunction.Length; i++)
+                    {
+                        if (Char.IsDigit(wordInFunction[i]))
+                            return (Convert.ToString(wordInFunction[i]));
+                    }
+                    return null;
+                }
+                Console.WriteLine("\nCapacity: {0}", myList.Capacity);
+                Console.WriteLine(myList[0].word + " " + myList[0].number);
+                Console.WriteLine(myList[1].word + " " + myList[1].number);
+                Console.WriteLine(myList[2].word + " " + myList[2].number);
+
+                //throw new NotImplementedException("mama");
             }
-            throw new NotImplementedException("mama");
         }
+    }
+
+    internal class wordList
+    {
+        public string word { get; set; }
+        public string number { get; set; }
     }
 }
