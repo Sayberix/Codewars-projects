e0048db9cb28cde4f4c014d9f7c74fdf2f685635
Maxim Konev <sayberix.ru@yandex.ru>
2022-08-28 00:09:06 +0700

    задача выполнена, контроль не пройден, нужжна оптимизация

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -17,18 +17,37 @@ namespace Codewars
         // Assert.AreEqual("23:59:59", TimeFormat.GetReadableTime(86399));
         // Assert.AreEqual("99:59:59", TimeFormat.GetReadableTime(359999));
 
-
         private static void Main(string[] args)
         {
-            Console.WriteLine($"{GetReadableTime(212)}");
+            Console.WriteLine($"{GetReadableTime(359999)}");
         }
 
         public static string GetReadableTime(int seconds)
         {
-
-
+            string[] array = new string[3] { "00", "00", "00" };
+            if (seconds > 0 && seconds < 60)
+            {
+                array[2] = Convert.ToString(seconds);
+            }
+            if (seconds > 59 && seconds < 3600)
+            {
+                array[2] = Convert.ToString(seconds % 60);
+                array[1] = Convert.ToString(seconds / 60);
+            }
+            if (seconds > 3599 && seconds < 360000)
+            {
+                array[0] = Convert.ToString(seconds / 60 / 60);
+                array[1] = Convert.ToString(seconds / 60 % 60);
+                array[2] = Convert.ToString(seconds % 60 % 60 % 60);
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (array[i].Length == 1)
+                {
+                    array[i] = string.Concat("0", array[i]);
+                }
+            }
+            return string.Concat("", array[0], ":", array[1], ":", array[2], "");
         }
-
     }
 }
-
