c58b219ca58be11c75626c08a4fe8c20212fac95
Maxim Konev <sayberix.ru@yandex.ru>
2022-08-27 14:28:31 +0700

    задача выполнена

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -15,13 +15,72 @@ namespace Codewars
 
         private static void Main(string[] args)
         {
-            Console.WriteLine($"{Rgb(148, 0, 211)}");
+            Console.WriteLine($"{Rgb(212, 53, 16)}");
         }
 
         public static string Rgb(int r, int g, int b)
         {
-            string HEX = string.Concat(r.ToString("X"), g.ToString("X"), b.ToString("X"));
-            return HEX;
+            Console.WriteLine(Convert.ToString(r) + " " + Convert.ToString(g) + " " + Convert.ToString(b));
+            int[] array = new int[3] { r, g, b };
+            string[] resaultArray = new string[3] { "00", "00", "00" };
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 15 && array[i] < 256)
+                {
+                    resaultArray[i] = array[i].ToString("X");
+                }
+                else
+                {
+                    if (array[i] > 0 && array[i] < 16)
+                    {
+                        resaultArray[i] = string.Concat("0",array[i].ToString("X"));
+                    }
+                    else
+                    {
+                        if (array[i] > 255)
+                        {
+                            resaultArray[i] = "FF";
+                        }
+                    }
+                }
+            }
+            return string.Join("", resaultArray);
         }
     }
 }
+
+/*
+ using System;
+
+public class Kata
+{
+  public static string Rgb(int r, int g, int b) 
+  {
+      r = Math.Max(Math.Min(255, r), 0);
+      g = Math.Max(Math.Min(255, g), 0);
+      b = Math.Max(Math.Min(255, b), 0);
+      return String.Format("{0:X2}{1:X2}{2:X2}", r, g, b);
+  }
+}
+
+using System;
+
+public class Kata
+{
+        public static string Rgb(int r, int g, int b)
+        {
+            return limitValue(r) + limitValue(g) + limitValue(b);
+        }
+
+        public static string limitValue(int n)
+        {
+            if (n < 0) n = 0;
+            if (n > 255) n = 255;
+            return n.ToString("X2");
+        }
+}
+ 
+ 
+ 
+ 
+ */
