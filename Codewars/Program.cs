53d96df537f2b90c99a87b0289e5d6cdb6b5f0f2
Maxim Konev <sayberix.ru@yandex.ru>
2022-09-28 22:53:13 +0700

    реализован собственный словарь азбуки Морзе методом MorseCode

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -9,13 +9,95 @@ namespace Codewars
 
         private static void Main(string[] args)
         {
-            
+            Console.Write(MorseCode(".."));
         }
 
+        /*
         public static string Decode(string morseCode)
         {
             Console.WriteLine(morseCode);
+                int length = morseCode.Length;
+                int [] array = new int [length];
+                for(int i = 0; i < length; i++)
+                  {
+                    array[i] = System.Convert.ToInt32(morseCode[i]);
+                  }
+            byte[] buffer = Encoding.ASCII.GetBytes(morseCode);
+            buffer = Encoding.Convert(Encoding.ASCII, Encoding.Default, buffer);
+            string result = Encoding.Default.GetString(buffer);
+            //Console.WriteLine();
 
+            byte[] bytes2 = BitConverter.GetBytes(i);
+            string s2 = Encoding.ASCII.GetString(bytes);
+            //string word = char.ConvertFromUtf32(array);
+            //Console.WriteLine(word);
+            return MorseCode.Get(result);
+        }*/
+
+        public static string MorseCode(string cod)
+        {
+            string resault;
+            switch (cod)
+            {
+                case "-.-.--": resault = "!"; break;
+                case ".-..-.": resault = "\""; break;
+                case "...-..-": resault = "$"; break;
+                case ".-...": resault = "&"; break;
+                case ".----.": resault = "\'"; break;
+                case "-.--.": resault = "("; break;
+                case "-.--.-": resault = ")"; break;
+                case ".-.-.": resault = "+"; break;
+                case "--..--": resault = ";"; break;
+                case "-....-": resault = "-"; break;
+                case ".-.-.-": resault = "."; break;
+                case "-..-.": resault = "/"; break;
+                case "-----": resault = "0"; break;
+                case ".----": resault = "1"; break;
+                case "..---": resault = "2"; break;
+                case "...--": resault = "3"; break;
+                case "....-": resault = "4"; break;
+                case ".....": resault = "5"; break;
+                case "-....": resault = "6"; break;
+                case "--...": resault = "7"; break;
+                case "---..": resault = "8"; break;
+                case "----.": resault = "9"; break;
+                case "---...": resault = ":"; break;
+                case "-.-.-.": resault = ";"; break;
+                case "-...-": resault = "="; break;
+                case "..--..": resault = "?"; break;
+                case ".--.-.": resault = "@"; break;
+                case ".-": resault = "A"; break;
+                case "-...": resault = "B"; break;
+                case "-.-.": resault = "C"; break;
+                case "-..": resault = "D"; break;
+                case ".": resault = "E"; break;
+                case "..-.": resault = "F"; break;
+                case "--.": resault = "G"; break;
+                case "....": resault = "H"; break;
+                case "..": resault = "I"; break;
+                case ".---": resault = "J"; break;
+                case "-.-": resault = "K"; break;
+                case ".-..": resault = "L"; break;
+                case "--": resault = "M"; break;
+                case "-.": resault = "N"; break;
+                case "---": resault = "O"; break;
+                case ".--.": resault = "P"; break;
+                case "--.-": resault = "Q"; break;
+                case ".-.": resault = "R"; break;
+                case "...": resault = "S"; break;
+                case "-": resault = "T"; break;
+                case "..-": resault = "U"; break;
+                case "...-": resault = "V"; break;
+                case ".--": resault = "W"; break;
+                case "-..-": resault = "X"; break;
+                case "-.--": resault = "Y"; break;
+                case "--..": resault = "Z"; break;
+                case "..--.-": resault = "_"; break;
+                case "...---...": resault = "SOS"; break;
+                default:
+                    resault = "-1"; break;
+            }
+            return resault;
         }
     }
 }
